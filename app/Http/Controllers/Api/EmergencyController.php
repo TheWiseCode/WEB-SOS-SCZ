12fb837293daa26dc13fc98e7efab15b001d45fc
Willy Vargas <wamdev01@gmail.com>
2021-12-06 23:05:05 -0400

    Send emergencies

--- app/Http/Controllers/Api/EmergencyController.php
+++ app/Http/Controllers/Api/EmergencyController.php
@@ -25,54 +25,6 @@ class EmergencyController extends Controller
         //
     }
 
-
-    /**
-     * @param String $
-     */
-    public function sendNotification(Emergency $emergency)
-    {
-        $url = 'https://fcm.googleapis.com/fcm/send';
-        $dataArr = array(
-            'click_action' => 'FLUTTER_NOTIFICATION_CLICK',
-            'id' => $emergency->id,
-            //'status'=>'done',
-            'Longitude' => $emergency->longitude,
-            'Latitude' => $emergency->latitude,
-        );
-        $notification = array(
-            'title' => $emergency->civilian->user->name . ' esta en peligro!',
-            'body' => $emergency->description . ' ' . Carbon::now(),
-            //'image'=> $req->img,
-            'sound' => 'default',
-            'badge' => '1',
-        );
-        $arrayToSend = array(
-//                'to' => 'fpLlqRvtR0maMLD36uLUyK:APA91bFuTXUKhN8PcP5_b7eU-SZnWKk5ugAaOAJB1eFEX6nLUehURoll_0UVSmr0-Mvcz7VJtcBM71122jpPj9FzMM2G7UIK80Rzcc9T8M0b2cZIOyGCPDPJsu1HfmWMelfidB6hx4fg', //podria ser NotificationDevide::all()->pluck('id');
-            'to' => '/topics/in_turn_operators',
-            'notification' => $notification,
-            'data' => $dataArr,
-            'priority' => 'high'
-        );
-        $fields = json_encode($arrayToSend);
-        $headers = array(
-            'Authorization: key=' . "AAAA5kWrVzo:APA91bFyafq8rsMSU0yvHsSSrMpjK1--rdcnVAy9mPfK8gZyCWXcCxc5SwlKIunZlNNaaeoqrmLsACOn04DANRkpkHpbicInRqaY30f2jFt71dMzE5jK4CxJGPJ8JqFcQrAWDgktYvAz",
-            'Content-Type: application/json',
-            "accept: */*"
-        );
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-        $response = curl_exec($ch);
-
-        curl_close($ch);
-
-        return $response;
-    }
-
     public function sendEmergency(Emergency $emergency)
     {
         $url = 'https://fcm.googleapis.com/fcm/send';
@@ -135,7 +87,7 @@ class EmergencyController extends Controller
         }
 
         $new_emergency = Emergency::create($request->validated());
-        $responde = $this->sendNotification($new_emergency);
+        $responde = $this->sendEmergency($new_emergency);
 
         return $responde;
         /*
